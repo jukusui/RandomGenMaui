53ffec01499923eb060279acf61f4b711b3c1e39
jukusui <jukusui@outlook.jp>
2024-12-17 00:28:58 +0900

    最低限のアプリ情報とバージョンを表示できるように

---
+++ Source/RandomGenMaui/Util/AssemblyInfoManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jukusui.RandomGen.Util;
+public static class AssemblyInfoManager
+{
+
+    static AssemblyInfoManager()
+    {
+        var asm = Assembly.GetCallingAssembly();
+        var name = asm.GetName();
+        Version = name.Version?.ToString() ?? "?.?.?";
+    }
+
+    public static string Version { get; }
+}
